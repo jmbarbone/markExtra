7a74b49b4336f145998e646f91df1b7446bd1665
Jordan Mark Barbone <jmbarbone@gmail.com>
2021-01-23 19:57:08 -0500

    add default_env()

---
+++ R/environment.R
@@ -0,0 +1,43 @@
+#' Default environment
+#'
+#' Creates an environment with just default packages
+#'
+#' @details
+#' A new environment is created with the parent of
+#'
+#' @param key A key for which environments to use
+#'
+#' @export
+default_env <- function(key = "default") {
+  e <- new.env(parent = baseenv())
+  switch(
+    key,
+    default = {
+      eval({
+        substitute({
+          requireNamespace("methods")
+          requireNamespace("stats")
+          requireNamespace("graphics")
+          requireNamespace("grDevices")
+          requireNamespace("utils")
+          requireNamespace("datasets")
+        },
+        env = e)
+      },
+      envir = e)
+    },
+    options = {
+      eval({
+        substitute({
+          for (i in getOption("defaultPackages")) {
+            requireNamespace(i)
+          }
+        },
+        env = e)
+      },
+      envir = e)
+    },
+    stop(key, " is not a valid key", call. = FALSE)
+  )
+  e
+}
