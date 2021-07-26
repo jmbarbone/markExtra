124b6b1435b63dbd0bc252c841d15e3db5d2d8e1
jmbarbone <jmbarbone@gmail.com>
2021-07-26 09:04:14 -0400

    additional cleanup

--- R/utils-pROC.R
+++ R/utils-pROC.R
@@ -87,7 +87,7 @@ subset_rownames <- function(x, y) {
 #'
 #' @export
 #' @examples
-#' if (require_namespace("pROC")) {
+#' if (mark::require_namespace("pROC")) {
 #'   x <- pROC::aSAH
 #'   mod <- pROC::roc(x$outcome, x$s100b, levels = c("Good", "Poor"))
 #'   pROC_quick_plot(mod)
