00698a8824c4b4c210eb3e1b9a59e7628d461f88
jmbarbone <jmbarbone@gmail.com>
2022-02-19 21:04:18 -0500

    add test

---
+++ tests/testthat/test-newer.R
@@ -0,0 +1,29 @@
+test_that("%newer% works", {
+  # make temp files and get times
+  f1 <- tempfile()
+  f2 <- tempfile()
+  f3 <- tempfile()
+  file.create(f1)
+  file.create(f2)
+  Sys.sleep(2)     # wait a couple seconds so the times are different
+  file.create(f3)
+
+  # get times for comparisons as well2
+  t1 <- file.mtime(f1)
+  t2 <- file.mtime(f2)
+  t3 <- file.mtime(f3)
+
+  expect_false(f1 %newer% t1)
+  expect_false(f2 %newer% t2)
+  expect_false(f3 %newer% t3)
+  expect_false(f1 %newer% f2)
+  expect_false(f1 %newer% f3)
+  expect_false(f3 %newer% f3)
+  expect_true( f3 %newer% f2)
+  expect_true( f3 %newer% f1)
+  expect_true( f1 %newer% tempfile())
+
+  expect_error(tempfile() %newer% f1, "exist")
+
+  file.remove(c(f1, f2, f3))
+})
