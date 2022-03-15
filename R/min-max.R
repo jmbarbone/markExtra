0b79283f9df6a519dbcde863216c96266be4d855
Jordan Mark Barbone <jbarbone@cogstate.com>
2022-03-15 11:20:49 -0400

    add missed tag

--- R/min-max.R
+++ R/min-max.R
@@ -6,6 +6,7 @@
 #'   and `1`, respectively.  `upper` must be greater than `lower`.
 #' @return A vector of x normalizes so the lower scores is `lower` and highest score is `upper`
 #' @export
+#' @examples
 #' x <- c(0.34, 0.24, 0.65, 0.21, 0.18, 0.79, 0.13, 0.94, 0.22, 0.1)
 #' min_max(x)
 #' min_max(x, upper = 10)
