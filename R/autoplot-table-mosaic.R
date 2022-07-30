8e7b4e95efb118702062d1f82f38c2bb6d4d53ce
jmbarbone <jmbarbone@gmail.com>
2022-07-29 21:25:13 -0400

    adds correctino for #6

--- R/autoplot-table-mosaic.R
+++ R/autoplot-table-mosaic.R
@@ -22,9 +22,20 @@ autoplot_table_mosaic <- function(x) {
 
   dn <- dimnames(x)
   nm <- names(dn)
+
+  rownames(x) <- as.numeric(factor(rownames(x)))
+  colnames(x) <- as.numeric(factor(colnames(x)))
+
+  df <- psych::table2df(x)
   # TODO this could probably be replaced
-  df <- psych::table2df(x, labs = nm)
-  df[] <- mapply(function(x, y) factor(x, levels = y), x = as.list(df), y = dn, SIMPLIFY = FALSE)
+  df <- psych::table2df(x)
+  colnames(df) <- nm
+  df[] <- mapply(
+    function(x, y) factor(x, levels = y),
+    x = as.list(df),
+    y = dn,
+    SIMPLIFY = FALSE
+  )
 
   x_arg <- rlang::sym(nm[1])
   y_arg <- rlang::sym(nm[2])
