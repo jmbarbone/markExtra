d0549701213f5a2785d883046bbdeb7803070a9a
Jordan Mark Barbone <jmbarbone@gmail.com>
2024-11-05 14:09:43 -0500

    improves examples

--- R/weighted-score.R
+++ R/weighted-score.R
@@ -9,13 +9,30 @@
 #' @param comparison A comparison value; a population mean or other value
 #' @examples
 #' scores <- rep(seq(0, 1, .1), each = 10)
-#' weights <- rep(1:10, 10)
+#' weights <- rep(1:10, 11)
 #' weighted <- weighted_score(scores, weights)
-#' plot(weighted, xlab = "inded", ylab = "Weighted score")
 #' plot(
-#'   weighted_score(
-#'     rep(seq(0, 1, .01))
-#'   )
+#'   weighted,
+#'   xlab = "index",
+#'   ylab = "Weighted score",
+#'   col = factor(scores),
+#'   pch = weights,
+#'   main = "Weighted score"
+#' )
+#' legend(
+#'   "bottomright",
+#'   legend = format(seq(0, 1, .1)),
+#'   col = 1:11,
+#'   pch = 1,
+#'   title = "Scores"
+#' )
+#' legend(
+#'   "bottomright",
+#'   inset = c(0.12, 0),
+#'   legend = format(1:10),
+#'   col = 1,
+#'   pch = weights,
+#'   title = "Weights"
 #' )
 #' @export
 weighted_score <- function(
